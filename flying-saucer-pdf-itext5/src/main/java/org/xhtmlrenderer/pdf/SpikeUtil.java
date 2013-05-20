24248b643f75de7abba5b2b7496831f4219d71ab
bw-il-md1 <bw-il-md1@asolutions.com>
2013-05-20 17:25:58 -0500

    219 - Improved bottom page image. Jake and Kartik

--- flying-saucer-pdf-itext5/src/main/java/org/xhtmlrenderer/pdf/SpikeUtil.java
+++ flying-saucer-pdf-itext5/src/main/java/org/xhtmlrenderer/pdf/SpikeUtil.java
@@ -2,17 +2,15 @@ package org.xhtmlrenderer.pdf;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
-import org.w3c.dom.Element;
-import org.w3c.dom.Text;
 import org.xhtmlrenderer.layout.LayoutContext;
 import org.xhtmlrenderer.render.BlockBox;
 import org.xhtmlrenderer.render.Box;
 import org.xhtmlrenderer.render.InlineLayoutBox;
-import org.xhtmlrenderer.render.InlineText;
-import org.xhtmlrenderer.render.LineBox;
 import org.xhtmlrenderer.render.PageBox;
 import org.xhtmlrenderer.render.RenderingContext;
 
@@ -23,83 +21,14 @@ public class SpikeUtil {
 	public static final int HALF_PAGE_IMAGE_HEIGHT = PAGE_HEIGHT / 2 + 40 - MARGIN_HEIGHT;
 	public static final int BOTTOM_HALF_PAGE_IMAGE_HEIGHT = HALF_PAGE_IMAGE_HEIGHT - 160;
 
-	public static void dump(LayoutContext c) {
-		Exception ex = new Exception("DUMP stack, not a problem");
-		ex.fillInStackTrace();
-		System.err.println("DUMP START");
-		// ex.printStackTrace();
-		try {
-			// dump(c.getRootLayer().getMaster(), c);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.err.println("DUMP END");
-	}
-
-	public static void dump(Box box, LayoutContext layoutContext) {
-		Element elem = box.getElement();
-		if (elem != null) {
-			System.out.println("ELEMENT: " + elem.toString());
-		}
-
-		StringBuffer sb = new StringBuffer();
-		RenderingContext renderContext = new RenderingContext(layoutContext.getSharedContext());
-
-		try {
-			box.collectText(renderContext, sb);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		System.out.println(box);
-		System.out.println("(" + box.getAbsX() + ", " + box.getAbsY() + ", " + box.getWidth() + "," + box.getHeight()
-				+ ") " + sb.toString());
-		if (box instanceof InlineLayoutBox) {
-			InlineLayoutBox inlineBox = (InlineLayoutBox) box;
-			handleInlineLayoutBox(inlineBox);
-		}
-		for (int index = 0; index < box.getChildCount(); index++) {
-			Box childBox = box.getChild(index);
-			dump(childBox, layoutContext);
-		}
-	}
-
-	private static void handleInlineLayoutBox(InlineLayoutBox inlineBox) {
-		for (int index = 0; index < inlineBox.getInlineChildCount(); index++) {
-			Object child = inlineBox.getInlineChild(index);
-			if (child instanceof InlineLayoutBox) {
-				InlineLayoutBox inlineBoxLayout = (InlineLayoutBox) child;
-				System.out.println("(" + inlineBoxLayout.getAbsX() + ", " + inlineBoxLayout.getAbsY() + ", "
-						+ inlineBoxLayout.getWidth() + "," + inlineBoxLayout.getHeight() + ") ");
-				handleInlineLayoutBox(inlineBoxLayout);
-			} else {
-				InlineText inlineText = (InlineText) child;
-				int x = inlineText.getX();
-				Text textNode = inlineText.getTextNode();
-				if (textNode != null) {
-					System.out.println(x + " TEXT:" + textNode.getTextContent());
-				}
-			}
-		}
-	}
-
-	public static void printPage(LineBox line, LayoutContext context) {
-		int page = context.getRootLayer().getFirstPage(context, line).getPageNo();
-		if (page == 5) {
-			System.err.println("Page 5!");
-		}
-		System.err.println("Line is on page: " + page + " (content)" + line.getTextDecorations());
-	}
-
 	public static void moveTextDown(final LayoutContext c) {
 		Box rootBox = c.getRootLayer().getMaster();
-		System.out.println("Move text down:");
 		final List topPageBoxes = new ArrayList();
 		final List bottomPageBoxes = new ArrayList();
 		visitAll(rootBox, new IBoxVisitor() {
 			public void visitBox(Box box) {
 				if (box instanceof BlockBox && box.getElement().getNodeName().equals("div")) {
 					int pageNumber = getPage(c, box);
-					System.out.println("box on page " + pageNumber);
 					StringBuffer sb = new StringBuffer();
 					RenderingContext renderContext = new RenderingContext(c.getSharedContext());
 					try {
@@ -120,32 +49,6 @@ public class SpikeUtil {
 			}
 		});
 
-		visitAll(rootBox, new IBoxVisitor() {
-			private Box findBox(final LayoutContext c, final List listBoxes, int pageNum) {
-				for (Iterator it = listBoxes.iterator(); it.hasNext();) {
-					Box box = (Box) it.next();
-					int boxPageNum = getPage(c, box);
-					if (boxPageNum == pageNum) {
-						return box;
-					}
-				}
-				return null;
-			}
-
-			public void visitBox(Box box) {
-				int pageNum = getPage(c, box);
-				Box topPageBox = findBox(c, topPageBoxes, pageNum);
-				if (topPageBox != null && box.getAbsY() < topPageBox.getAbsY()) {
-					box.setAbsY(box.getAbsY() + HALF_PAGE_IMAGE_HEIGHT);
-				}
-				Box bottomPageBox = findBox(c, bottomPageBoxes, pageNum);
-				if (bottomPageBox != null && box.getAbsY() > bottomPageBox.getAbsY() + bottomPageBox.getHeight()) {
-					System.out.println("Moving text behind bottom pic:" + box);
-					box.setAbsY(box.getAbsY() - HALF_PAGE_IMAGE_HEIGHT);
-				}
-			}
-		});
-
 		// Move top pictures up
 		for (Iterator it = topPageBoxes.iterator(); it.hasNext();) {
 			Box box = (Box) it.next();
@@ -160,6 +63,60 @@ public class SpikeUtil {
 			int yDiff = newY - box.getAbsY();
 			moveAll(box, yDiff);
 		}
+
+		// Move half-lines
+		final Map halfLineAdjustments = new HashMap();
+		visitAll(rootBox, new IBoxVisitor() {
+			public void visitBox(Box box) {
+				int pageNum = getPage(c, box);
+				Box bottomPageBox = findBox(c, bottomPageBoxes, pageNum);
+				if (!boxTopOverlapsWithBox(box, bottomPageBox) && boxTopOverlapsWithBox(bottomPageBox, box)
+						&& box != bottomPageBox && !(box instanceof BlockBox)) {
+					System.out.println("Moving partially covered text behind bottom pic:" + box);
+					int adjustment = bottomPageBox.getAbsY() - box.getAbsY();
+					box.setAbsY(box.getAbsY() + HALF_PAGE_IMAGE_HEIGHT + adjustment);
+					halfLineAdjustments.put(bottomPageBox, new Integer(adjustment));
+				}
+			}
+		});
+
+		// Moving text and other stuff around the pictures
+		visitAll(rootBox, new IBoxVisitor() {
+
+			public void visitBox(Box box) {
+				int pageNum = getPage(c, box);
+				Box topPageBox = findBox(c, topPageBoxes, pageNum);
+				if (boxTopOverlapsWithBox(box, topPageBox) && box != topPageBox) {
+					System.out.println("Moving text behind top pic:" + box);
+					box.setAbsY(box.getAbsY() + HALF_PAGE_IMAGE_HEIGHT);
+				}
+				Box bottomPageBox = findBox(c, bottomPageBoxes, pageNum);
+				if (boxTopOverlapsWithBox(box, bottomPageBox) && box != bottomPageBox) {
+					Integer adjustment = (Integer) halfLineAdjustments.get(bottomPageBox);
+					if (adjustment == null) {
+						adjustment = new Integer(0);
+					}
+					System.out.println("Moving text behind bottom pic:" + box);
+					box.setAbsY(box.getAbsY() + HALF_PAGE_IMAGE_HEIGHT + adjustment.intValue());
+				}
+			}
+		});
+	}
+
+	private static Box findBox(final LayoutContext c, final List listBoxes, int pageNum) {
+		for (Iterator it = listBoxes.iterator(); it.hasNext();) {
+			Box box = (Box) it.next();
+			int boxPageNum = getPage(c, box);
+			if (boxPageNum == pageNum) {
+				return box;
+			}
+		}
+		return null;
+	}
+
+	private static boolean boxTopOverlapsWithBox(Box box, Box topPageBox) {
+		return topPageBox != null && box != null && box.getAbsY() < topPageBox.getAbsY() + topPageBox.getHeight()
+				&& box.getAbsY() > topPageBox.getAbsY();
 	}
 
 	private static void moveAll(Box box, final int yDiff) {
